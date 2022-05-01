258fb9522de49cbe2b84dcb91a90a7c45d5a4630
Mohamed Elsheikh <55459616+Mo-Elsheikh@users.noreply.github.com>
2022-05-01 14:10:10 -0600

    Update assign5.py
    fiexd

--- assign5.py
+++ assign5.py
@@ -2,8 +2,7 @@
 For this assignment there is no automated testing. You will instead submit
 your *.py file in Canvas. I will download and test your program from Canvas.
 """
-import random
-import math
+
 import time
 import sys
 INF = sys.maxsize
@@ -32,12 +31,13 @@ def adjMatFromFile(filename):
 
 def TSPwGenAlgo(
         g,
-        max_num_generations=100,
-        population_size=200,
+        max_num_generations=5,
+        population_size=10,
         mutation_rate=0.01,
         explore_rate=0.5
     ):
     """ A genetic algorithm to attempt to find an optimal solution to TSP  """
+
     # NOTE: YOU SHOULD CHANGE THE DEFAULT PARAMETER VALUES ABOVE TO VALUES YOU
     # THINK WILL YIELD THE BEST SOLUTION FOR A GRAPH OF ~100 VERTS AND THAT CAN
     # RUN IN 5 MINUTES OR LESS (ON AN AVERAGE LAPTOP COMPUTER)
@@ -45,35 +45,31 @@ def TSPwGenAlgo(
     solution_path = [] # list of n+1 verts representing sequence of vertices with lowest total distance found
     solution_distance = INF # distance of final solution path, note this should include edge back to starting vert
     avg_path_each_generation = [] # store average path length path across individuals in each generation
-    number_of_cities = len(g)  
 
-    discover_size = math.ceil(explore_rate * population_size)
-    mate = int(population_size)
-    population = generate_initial_population(population_size, number_of_cities) # create individual members of the population  
-    for n in range(max_num_generations):
-        for i in range(population_size):
-            pop_result = (calc_distance_for_given_path(population[i][1], g), population[i][1])
-            population[i] = pop_result
-        population.sort(key=lambda city: city[0])
-        avg_path_each_generation.append(population[0][0])
-        children = []
-        for i in range(population_size):
-            parent_1 = mating(population[:discover_size], mate)
-            parent_2 = mating(population[:discover_size], mate)
-            children.append((0, crossover(parent_1[1], parent_2[1])))
-        for swap_item1 in range(len(children)):  # Doing the mutation
-            rand_val = random.random()
-            if mutation_rate > rand_val:
-                swap_item2 = random.randrange(number_of_cities)
-                child1_temp = children[swap_item1]
-                children[swap_item1] = children[swap_item2]
-                children[swap_item2] = child1_temp
-        population = children
-  
-    population.sort(key=lambda city: city[0])
-    solution_path = population[0][1]
-    solution_distance = calc_distance_for_given_path(population[0][1], g)
-    solution_path = [population[0][1], solution_path[0]]
+    # create individual members of the population
+
+    # initialize individuals to an initial 'solution'
+
+    # loop for x number of generations (can also choose to add other early-stopping criteria)
+
+        # calculate fitness of each individual in the population
+
+        # calculate average path length across individuals in this generation
+        # and store in avg_path_each_generation
+
+        # select the individuals to be used to spawn the generation, then create
+        # individuals of the new generation (using some form of crossover)
+
+        # allow for mutations (shuold be based on mutation_rate, should not happen too often)
+
+        # ...
+
+    # calculate and *verify* final solution
+
+    # update solution_path and solution_distance
+
+    # ...
+
     return {
             'solution_path': solution_path,
             'solution_distance': solution_distance,
@@ -81,59 +77,6 @@ def TSPwGenAlgo(
            }
 
 
-def generate_initial_population(size, number_of_cities):
-    """ Method to generate the initial population """
-    initial_population = []
-    for i in range(size):
-        initial_population.append((0, random.sample(range(number_of_cities), number_of_cities)))
-    return initial_population
-
-
-def calc_distance_for_given_path(path, graph):
-    """ Method to get distance within a path """
-    distance = 0
-    for i in range(-1,(len(path) - 1), 1):
-        distance += graph[path[i]][path[i + 1]]
-    return distance
-
-
-def crossover(parent1, parent2):
-    """ Method to do the crossover """
-    children = []
-    child1 = []
-    child2 = []
-    
-    first = int(random.random() * len(parent1))
-    second = int(random.random() * len(parent1))
-    # Selecting the start and end gene
-    start_gene = first
-    end_gene = second
-    if (second < first):
-        start_gene = first
-        end_gene = first
-    for i in range(start_gene, end_gene):
-        child1.append(parent1[i])
-    for element in parent2:
-        if element not in child1:
-            child2.append(element)
-    children = child1 + child2
-    return children
-
-
-def mating(population, mating_size):
-    """ Method to execute the Mating """
-    mate_list = []
-
-    for i in range(mating_size):
-        pop_rand_val = population[random.randrange(len(population))]
-        mate_list.append(pop_rand_val)
-    #print (mate_list[0], mate_list[1], mate_list[-1])
-    min_mate_val = mate_list[0]
-    for mate in mate_list:
-        if mate[0] < min_mate_val[0]:
-            min_mate_val = mate
-    return min_mate_val
-   
 def TSPwDynProg(g):
     """ (10pts extra credit) A dynamic programming approach to solve TSP """
     solution_path = [] # list of n+1 verts representing sequence of vertices with lowest total distance found
@@ -162,7 +105,7 @@ def TSPwBandB(g):
 
 def assign05_main():
     """ Load the graph (change the filename when you're ready to test larger ones) """
-    g = adjMatFromFile("complete_graph_n100.txt")
+    g = adjMatFromFile("complete_graph_n08.txt")
 
     # Run genetic algorithm to find best solution possible
     start_time = time.time()
